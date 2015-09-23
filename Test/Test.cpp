b69e55f613a9a639f9ff749c1aba39b26d341a70
Brett Cannon <brett@python.org>
2015-09-23 16:03:55 -0700

    Test bool/list binary operation type inference

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1977,6 +1977,28 @@ void AbsIntTest() {
                 new VariableVerifier(22, 0, AVK_Integer)            // x assigned in-place
             }
         ),
+        // Bool/list
+        AITestCase(
+            "def f():\n    x = True\n    y = []\n    z = x * y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_Bool),               // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_List),              // y assigned
+                new VariableVerifier(19, 2, AVK_Undefined, true),   // z not assigned yet
+                new VariableVerifier(22, 2, AVK_List),              // z assigned
+            }
+        ),
+        AITestCase(
+            "def f():\n    x = True\n    y = []\n    x *= y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_Bool),               // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_List),              // y assigned
+                new VariableVerifier(22, 0, AVK_List)               // x assigned in-place
+            }
+        ),
         
         // Complex binary operations
         // Complex/bool
