1afd910ecb1741a98018c56bad378ccf674166d8
Brett Cannon <brett@python.org>
2015-09-24 15:08:01 -0700

    Test int/str operation type inference

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1943,6 +1943,28 @@ void AbsIntTest() {
                 new VariableVerifier(22, 0, AVK_List)               // x assigned in-place
             }
         ),
+        // Int/str
+        AITestCase(
+            "def f():\n    x = 42\n    y = ''\n    z = x * y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_Integer),            // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_String),            // y assigned
+                new VariableVerifier(19, 2, AVK_Undefined, true),   // z not assigned yet
+                new VariableVerifier(22, 2, AVK_String),            // z assigned
+            }
+        ),
+        AITestCase(
+            "def f():\n    x = 42\n    y = ''\n    x *= y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_Integer),            // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_String),            // y assigned
+                new VariableVerifier(22, 0, AVK_String)             // x assigned in-place
+            }
+        ),
 
         // Float binary operations
         AITestCase(
