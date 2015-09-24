1171d068cbbc48bfe54f02e48e48d90268f7373d
Brett Cannon <brett@python.org>
2015-09-24 15:09:09 -0700

    Test int/tuple operation type inference

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1965,6 +1965,28 @@ void AbsIntTest() {
                 new VariableVerifier(22, 0, AVK_String)             // x assigned in-place
             }
         ),
+        // Int/tuple
+        AITestCase(
+            "def f():\n    x = 42\n    y = ()\n    z = x * y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_Integer),            // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_Tuple),             // y assigned
+                new VariableVerifier(19, 2, AVK_Undefined, true),   // z not assigned yet
+                new VariableVerifier(22, 2, AVK_Tuple),             // z assigned
+            }
+        ),
+        AITestCase(
+            "def f():\n    x = 42\n    y = ()\n    x *= y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_Integer),            // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_Tuple),             // y assigned
+                new VariableVerifier(22, 0, AVK_Tuple)              // x assigned in-place
+            }
+        ),
 
         // Float binary operations
         AITestCase(
