9903948b8bd53bce9b6792476dcfa7d38e249e6d
Brett Cannon <brett@python.org>
2015-09-28 11:47:33 -0700

    Test type inference of list/int binary operations

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -4118,6 +4118,28 @@ void AbsIntTest() {
                 new VariableVerifier(22, 0, AVK_List)               // x assigned in-place
             }
         ),
+        // List/int
+        AITestCase(
+            "def f():\n    x = []\n    y = 42\n    z = x * y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_List),               // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_Integer),           // y assigned
+                new VariableVerifier(19, 2, AVK_Undefined, true),   // z not assigned yet
+                new VariableVerifier(22, 2, AVK_List),              // z assigned
+            }
+        ),
+        AITestCase(
+            "def f():\n    x = []\n    y = 42\n    x *= y",
+            {
+                new VariableVerifier(3, 0, AVK_Undefined, true),    // x not assigned yet
+                new VariableVerifier(6, 0, AVK_List),               // x assigned
+                new VariableVerifier(9, 1, AVK_Undefined, true),    // y not assigned yet
+                new VariableVerifier(12, 1, AVK_Integer),           // y assigned
+                new VariableVerifier(22, 0, AVK_List)               // x assigned in-place
+            }
+        ),
 
         // Unary not
         AITestCase(
