4abb9b0751e9cf681ed131de6fdfdf88d26374cb
Brett Cannon <brett@python.org>
2016-01-21 15:57:19 -0800

    Make CoreCLR a git submodule

--- GetDeps.bat
+++ GetDeps.bat
@@ -1,21 +1,9 @@
 @echo off
 REM ########################################################
-REM # This script downloads CoreCLR and the CPython code bases
-REM # so that you can build them and use them with Pyjion.
-REM # These are locked back to specific versions currently.
-
-echo Grabbing CoreCLR...
-
-REM ########################################################
-REM # Clone CoreCLR code base
-git clone https://github.com/dotnet/coreclr CoreCLR
-pushd CoreCLR
-git reset --hard b16ff5935ff9df3211798f18f0c951666ae27774
-
-REM ########################################################
 REM # Apply changes to disable COM interop support
 
-echo Disabling COM interop support...
+echo Disabling COM interop support in CoreCLR...
+pushd CoreCLR
 git apply ..\Patches\CoreCLR\src\inc\utilcode.h
 git apply ..\Patches\CoreCLR\src\utilcode\CMakeLists.txt
 git apply ..\Patches\CoreCLR\src\utilcode\util.cpp
@@ -30,7 +18,7 @@ popd
 REM ########################################################
 REM # Apply changes to integrate JIT support into CPython
 
-echo Applying changes to enable JIT support...
+echo Applying changes to enable JIT support in Python...
 pushd Python
 git apply ..\Patches\python.diff
 popd
