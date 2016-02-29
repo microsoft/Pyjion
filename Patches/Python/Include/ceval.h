ccc5ae4a4717034abf63444e1a61564685d7dc2b
Brett Cannon <brett@python.org>
2016-02-29 13:05:17 -0800

    Fix Include/ceval.h patch

--- Patches/Python/Include/ceval.h
+++ Patches/Python/Include/ceval.h
@@ -1,5 +1,5 @@
 diff --git a/Include/ceval.h b/Include/ceval.h
-index b5373a9..59c1171 100644
+index ec62571..71b904d 100644
 --- a/Include/ceval.h
 +++ b/Include/ceval.h
 @@ -10,6 +10,11 @@ extern "C" {
@@ -14,8 +14,8 @@ index b5373a9..59c1171 100644
  /* Inline this */
  #define PyEval_CallObject(func,arg) \
      PyEval_CallObjectWithKeywords(func, arg, (PyObject *)NULL)
-@@ -82,6 +87,11 @@ PyAPI_FUNC(int) Py_GetRecursionLimit(void);
- PyAPI_FUNC(int) _Py_CheckRecursiveCall(const char *where);
+@@ -82,6 +87,12 @@ PyAPI_FUNC(int) Py_GetRecursionLimit(void);
+ PyAPI_FUNC(int) _Py_CheckRecursiveCall(char *where);
  PyAPI_DATA(int) _Py_CheckRecursionLimit;
  
 +/* Checks to see if periodic work needs to be done, such as releasing the GIL,
@@ -23,10 +23,11 @@ index b5373a9..59c1171 100644
 +   if an error occurred or false on success. */
 +PyAPI_FUNC(int) _PyEval_PeriodicWork(void);
 +
++
  #ifdef USE_STACKCHECK
  /* With USE_STACKCHECK, we artificially decrement the recursion limit in order
     to trigger regular stack checks in _Py_CheckRecursiveCall(), except if
-@@ -119,6 +129,7 @@ PyAPI_FUNC(const char *) PyEval_GetFuncDesc(PyObject *);
+@@ -113,6 +124,7 @@ PyAPI_FUNC(const char *) PyEval_GetFuncDesc(PyObject *);
  PyAPI_FUNC(PyObject *) PyEval_GetCallStats(PyObject *);
  PyAPI_FUNC(PyObject *) PyEval_EvalFrame(struct _frame *);
  PyAPI_FUNC(PyObject *) PyEval_EvalFrameEx(struct _frame *f, int exc);
