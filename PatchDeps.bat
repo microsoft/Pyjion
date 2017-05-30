4c86f45015ea4ede17db439667c4e9aa14174896
Dino Viehland <dinov@microsoft.com>
2017-05-30 16:55:17 -0700

    Remove Python patches

--- PatchDeps.bat
+++ PatchDeps.bat
@@ -15,18 +15,3 @@ git apply ..\Patches\CoreCLR\clr.defines.targets
 git apply ..\Patches\CoreCLR\clr.desktop.props
 git apply ..\Patches\CoreCLR\CMakeLists.txt
 popd
-
-REM ########################################################
-REM # Apply changes to integrate JIT support into CPython
-
-echo Applying changes to enable JIT support in Python...
-pushd Python
-git apply ..\Patches\Python\Include\ceval.h
-git apply ..\Patches\Python\Include\code.h
-git apply ..\Patches\Python\Include\dictobject.h
-git apply ..\Patches\Python\Include\pystate.h
-git apply ..\Patches\Python\Objects\codeobject.c
-git apply ..\Patches\Python\Python\ceval.c
-git apply ..\Patches\Python\Python\pylifecycle.c
-git apply ..\Patches\Python\Python\pystate.c
-popd
--- PatchDeps.sh
+++ PatchDeps.sh
@@ -11,15 +11,3 @@ git apply ../Patches/CoreCLR/clr.defines.targets
 git apply ../Patches/CoreCLR/clr.desktop.props
 git apply ../Patches/CoreCLR/CMakeLists.txt
 popd
-
-echo Applying changes to enable JIT support in Python...
-pushd Python
-git apply ../Patches/Python/Include/ceval.h
-git apply ../Patches/Python/Include/code.h
-git apply ../Patches/Python/Include/dictobject.h
-git apply ../Patches/Python/Include/pystate.h
-git apply ../Patches/Python/Objects/codeobject.c
-git apply ../Patches/Python/Python/ceval.c
-git apply ../Patches/Python/Python/pylifecycle.c
-git apply ../Patches/Python/Python/pystate.c
-popd
