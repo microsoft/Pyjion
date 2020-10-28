da767db4e48679c035f3e31c6200c818abe37043
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-10-28 15:16:49 +1100

    Output search path when loading module

--- pyjion/__init__.py
+++ pyjion/__init__.py
@@ -3,11 +3,13 @@ import pathlib
 import os
 import platform
 
-def _no_dotnet():
-    print("Can't find a .NET 5 installation, "
+
+def _no_dotnet(path):
+    print(f"Can't find a .NET 5 installation in {path}, "
           "provide the DOTNET_ROOT environment variable "
           "if its installed somewhere unusual")
     exit(1)
+
 
 # try and locate .Net 5
 def _which_dotnet():
@@ -15,23 +17,26 @@ def _which_dotnet():
     if 'DOTNET_ROOT' in os.environ:
         _dotnet_root = pathlib.Path(os.environ['DOTNET_ROOT'])
     if not _dotnet_root.exists():
-        _no_dotnet()
+        _no_dotnet(_dotnet_root)
     if platform.system() == "Darwin":
         lib_path = list(_dotnet_root.glob('Microsoft.NETCore.App*/5.0.0*/libclrjit.dylib'))
         if len(lib_path) > 0:
             clrjitlib = str(lib_path[0])
             ctypes.cdll.LoadLibrary(clrjitlib)
         else:
-            _no_dotnet()
+            _no_dotnet(_dotnet_root)
     elif platform.system() == "Linux":
         lib_path = list(_dotnet_root.glob('Microsoft.NETCore.App*/5.0.0*/libclrjit.so'))
         if len(lib_path) > 0:
             clrjitlib = str(lib_path[0])
             ctypes.cdll.LoadLibrary(clrjitlib)
         else:
-            _no_dotnet()
+            _no_dotnet(_dotnet_root)
+    else:
+        # TODO : Implement Windows
+        return
+
 
 _which_dotnet()
 
-from ._pyjion import *
-
+from ._pyjion import *  # NOQA
