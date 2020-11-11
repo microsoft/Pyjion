0c57c85630d364b1e458c25e3da3722e3eb79963
Anthony Shaw <anthony.p.shaw@gmail.com>
2020-11-11 11:17:16 +1100

    Set the OS page size so that the EE doesnt' request a massive stack frame and cause an overflow exception

--- pyjion/helpers.asm
+++ pyjion/helpers.asm
@@ -1,4 +1,5 @@
 .code
+PAGE_SIZE equ 1000h
 JIT_StackProbe PROC
 
         ; On entry:
@@ -11,11 +12,11 @@ JIT_StackProbe PROC
         ; NOTE: this helper will probe at least one page below the one pointed by rsp.
 
         mov     rax, rsp               ; rax points to some byte on the last probed page
-        and     rax, -1000h        ; rax points to the **lowest address** on the last probed page
+        and     rax, -PAGE_SIZE         ; rax points to the **lowest address** on the last probed page
                                        ; This is done to make the following loop end condition simpler.
 
 ProbeLoop:
-        sub     rax, 1000h         ; rax points to the lowest address of the **next page** to probe
+        sub     rax, PAGE_SIZE         ; rax points to the lowest address of the **next page** to probe
         test    dword ptr [rax], eax   ; rax points to the lowest address on the **last probed** page
         cmp     rax, r11
         jg      ProbeLoop              ; If (rax > r11), then we need to probe at least one more page.
